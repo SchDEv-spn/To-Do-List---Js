dd23c3d68ddf2548d538dd432df5f5e3720bb8cc
schneider manrique <schneider.manrique@gmai.com>
2025-09-17 16:26:15 -0500

    defini las variables globales

---
+++ js/main.js
@@ -0,0 +1,8 @@
+const input = document.getElementById("todo-form")
+
+
+const addBtn= document.getElementById("add-btn")
+
+const todoLis= document.getElementById("item-to-do")
+
+const completeList= document.getElementById("cont-completed-list")
