27f63098da004c8e0199ef5b336b2f9283bfe92e
schneider manrique <schneider.manrique@gmai.com>
2025-09-17 17:16:30 -0500

    cree la funcion que genera el nodo padre de la tarea con sus hijos , usando js

--- js/main.js
+++ js/main.js
@@ -1,8 +1,33 @@
-const input = document.getElementById("todo-form")
+const input = document.getElementById("todo-form");
+const addBtn = document.getElementById("add-btn");
+const toDoLis = document.getElementById("item-to-do");
+const completeList = document.getElementById("cont-completed-list");
 
 
-const addBtn= document.getElementById("add-btn")
+// creamos la funcion que nos permite crear una nueva tarea apartir del formulario/
+// toda etiqueta que vamos a crear es apartir de la maqueta html pre-existente/
 
-const todoLis= document.getElementById("item-to-do")
+function createToDoItem(textoItem) {
+    // creamos el nodo o elemento padre o contenedor
+    const item = document.createElement('div');
+    item.classList.add('item-to-do')
+    // creamos el nodo hijo input y le gregamos el type checkbox
+    const checkbox = document.createElement('input')
+    checkbox.type = 'checkbox';
 
-const completeList= document.getElementById("cont-completed-list")
+    // creamos el siguiente nodo hijo parrafo en este caso/
+    // a este parafo le asigno el valor del argumento de la funcion , es decir lo que escribe el usuario en el campo del formulario
+
+    const p = document.createElement('p');
+    p.textContent = textoItem
+    // creamos el ultimo nodo hijo para el boton de eliminar
+    const deleteBtn = document.createElement('button')
+    deleteBtn.textContent = 'Eliminar';
+    // ensamblamos dentro del nodo padre , sus nodos hijos, es deecier la estructura de la tarea
+
+    item.appendChild(checkbox);
+    item.appendChild(p);
+    item.appendChild(deleteBtn);
+    // utilizamos el return coon el fin de dar respuesta del elemneto creado, esta se usara mas adelante 
+    return item;
+}
